fb79e1fdfb9b55f02e0a351bc6eeb0f02c332b83
jq-wei <jieqiang.wei.dev@gmail.com>
2025-06-02 15:04:14 +0000

    Deploying to gh-pages from @ jq-wei/jq-wei.github.io@260c5312ecdf84df3a006b2f863bff04b5237399 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -504,6 +504,13 @@ ninja.data = [{
           window.open("mailto:%6A%69%65%71%69%61%6E%67.%77%65%69.%64%65%76@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/jq-wei", "_blank");
+        },
+      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
